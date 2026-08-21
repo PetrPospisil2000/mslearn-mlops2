2520b498afd22446b3b0fe61fe7984b1d24818a4
PetrPospisil2000 <p.po@outlook.de>
2026-08-21 03:57:01 -0700

    Change unique suffix for endpoint naming
    Replaced dynamic unique suffix with a static value.

--- src/deploy_to_online_endpoint.py
+++ src/deploy_to_online_endpoint.py
@@ -49,7 +49,7 @@ def ensure_endpoint(ml_client: MLClient, endpoint_name: str) -> ManagedOnlineEnd
     try:
         return ml_client.online_endpoints.get(name=endpoint_name)
     except ResourceNotFoundError:
-        unique_suffix = datetime.datetime.now().strftime("%m%d%H%M%S")
+        unique_suffix = "62120386"
         endpoint = ManagedOnlineEndpoint(
             name= f"{endpoint_name}-{unique_suffix}",
             description="Online endpoint for MLflow diabetes model",
